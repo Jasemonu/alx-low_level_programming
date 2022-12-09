3c8027c4ad202d74c4d99f55f8ae84ea5c1b28e7
Jasemonu <jasemonu@gmail.com>
2022-12-08 16:41:31 -0800

    Data sizes types in C programmming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,19 @@
  */
 int main(void)
 {
-	
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of long long %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of float %lu byte(s)\n", (unsigned long)sizeof(e));
+
 	return (0);
 }
+
+
