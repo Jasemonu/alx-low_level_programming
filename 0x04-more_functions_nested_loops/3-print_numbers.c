28ddc0a29c4d2d9692e650e698858a612a14c28b
Jasemonu <jasemonu@gmail.com>
2022-12-15 07:20:16 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,12 @@
  */
 void print_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 0; c < 10; i++)
 	{
-		_putchar(c);
+
+		_putchar(i + '0');
 	}
 	_putchar ('\n');
 }
