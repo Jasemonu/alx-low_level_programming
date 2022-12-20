f0e2e84702bec7fbf32de7595e1493e22fffdf78
Jasemonu <jasemonu@gmail.com>
2022-12-20 00:53:46 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,23 @@
 
 void puts2(char *str)
 {
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
+	while (*y != '\0')
 	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-
-		_putchar('\n');
+		y++;
+		longi++;
 	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
 }
