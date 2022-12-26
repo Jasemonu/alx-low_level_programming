af60ba67cf2cef6c8820da316a738f8b935570fa
Jasemonu <jasemonu@gmail.com>
2022-12-26 10:15:44 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,11 @@ char *_strcat(char *dest, char *src)
 	{
 		dest++;
 	}
-	while (*src !='\0')
+	while (*src != '\0')
 	{
 		*dest = *src;
-		dest++
-		src++
+		dest++;
+		src++;
 	}
 	*dest = '\0';
 	return (s);
