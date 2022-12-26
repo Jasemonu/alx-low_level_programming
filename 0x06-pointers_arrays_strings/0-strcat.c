fa42c6cfc943fbe9ab4e151f03ab77b5db7b5efb
Jasemonu <jasemonu@gmail.com>
2022-12-26 02:23:23 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*char *_strcat - a function that concatenates two strings.
+*@dest: First parameter declaration
+*@src: Second  parameter declaration
+*
+*Return: 0;
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char dest[];
+	char src[];
+	char *ptr;
+
+	for (i = 0; dest[i] < != '\0'; i++)
+	{
+		_putchar(dest[i]);
+
+		for (j = 0; src[i] < != '\0'; j++)
+		{
+			_putchar(src[j]);
+		}
+		ptr = (dest[i] + src[j])
+			_putchar(ptr);
+	}
+	return (0);
+}
