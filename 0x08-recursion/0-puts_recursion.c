ae5ffce7dc3d5d7cf52ca273a03fe269b07a2864
Jasemonu <jasemonu@gmail.com>
2023-01-05 14:26:24 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,7 +4,7 @@
  *_puts_recursion - A function that prints a string followed by a new line
  *@s: declaration of the string
  *
- *Return: void
+ *Return: success
  */
 
 void _puts_recursion(char *s)
