8c6cc094cf6a9e79d326ab4aa510ee133ecf7aa7
Jasemonu <jasemonu@gmail.com>
2023-01-07 12:21:13 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -33,9 +33,10 @@ void print_line(char *c, int s, int l)
 }
 
 /**
- * print_buffer - prints a buffer
+ *print_buffer - prints a buffer
  *@b: buffer to print
  *@size: size of buffer
+ *
  *Return: void
  */
 void print_buffer(char *b, int size)
@@ -48,6 +49,10 @@ void print_buffer(char *b, int size)
 		if (i < size / 10)
 		{
 			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
 		}
 		putchar('\n');
 	}
