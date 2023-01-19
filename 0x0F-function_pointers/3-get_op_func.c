af2fe2eaded40ef38890b1363beb583373302b9e
Jasemonu <jasemonu@gmail.com>
2023-01-19 02:22:41 -0800

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,12 +17,16 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
 
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
+	{
 		return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 
 }
