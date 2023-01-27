33be8476f02f1257b235a3463b2b006d71ab4b1a
Jasemonu <jasemonu@gmail.com>
2023-01-27 01:35:56 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,11 +14,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			 printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		 s++;
+		s++;
 	}
 	return (s);
 }
