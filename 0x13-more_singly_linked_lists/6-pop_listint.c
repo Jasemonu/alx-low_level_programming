3a3f76df129c52829156027b992887007a592edc
Jasemonu <jasemonu@gmail.com>
2023-01-31 02:05:34 -0800

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,8 +15,8 @@ int pop_listint(listint_t **head)
 	if (!head || !*head)
 		return (0);
 
-		num = (*head)->n;
-		temp = (*head)->next;
+		num = (*head);
+		tem = (*head)->next;
 		free(*head);
 		*head = temp;
 
